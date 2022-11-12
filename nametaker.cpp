f50af9799e462cd7cab3a21002ba2b0b4be505b0
JulienM14 <julienmedina314@gmail.com>
2022-11-12 18:10:50 -0500

    Initial commit

---
+++ nametaker.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string userName;
+    cout << "Hello! \n";
+    cout << "Please input your name! \n";
+    cin >> userName;
+    cout << "Your name is " << userName;
+    cout << "running this on windows is going to fucking suck";
+}
+
