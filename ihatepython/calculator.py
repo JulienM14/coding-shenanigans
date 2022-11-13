3bed0c139a8b44f1e5dea7960e74743d7b3edc44
JulienM14 <julienmedina314@gmail.com>
2022-11-13 16:48:26 -0500

    cool

---
+++ ihatepython/calculator.py
@@ -0,0 +1,32 @@
+def numbergrabber():
+    print("Input a number! \n")
+    number = int(input(" > "))
+    return number
+
+
+def functiongrabber():
+    print("Input a function! \n")
+    print("Addition (1), Subtraction (2) \n")
+    print("Mulitplication (3), Division (4) \n")
+    function = int(input(" > "))
+    return function
+
+
+def mathcompleter(x, y, function):
+    if function == 1:
+        answer = x + y
+    elif function == 2:
+        answer = x - y
+    elif function == 3:
+        answer = x * y
+    elif function == 4:
+        answer = x / y
+    else:
+        answer = 0
+    return answer
+
+x = numbergrabber()
+y = numbergrabber()
+function = functiongrabber()
+answer = mathcompleter(x, y, function)
+print("Your answer is", answer)
